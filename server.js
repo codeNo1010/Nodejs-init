67293669ca66fedb59b30ece8aaac3bc07b6b972
endea <codeno.1010@gmail.com>
2022-12-02 15:12:10 +0900

    node init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){
+    console.log('listening on 8080')
+});
+
+app.get('/first' , function(요청, 응답){
+    응답.send('처음 여는 /first 입니다.');
+});
+
+// 들어온 요청을 HTML로 보내기
+app.get('/', function(요청, 응답){
+    응답.sendFile(__dirname + '/index.html');
+});
